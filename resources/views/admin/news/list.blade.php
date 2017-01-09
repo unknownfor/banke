cec476b0460815401dfefc8c95903fa2a337251f
shaolei <ysl90s@qq.com>
2017-01-09 14:08:56 +0800

    fix

--- resources/views/admin/news/list.blade.php
+++ resources/views/admin/news/list.blade.php
@@ -135,7 +135,7 @@
 <script type="text/javascript" src="{{asset('backend/plugins/datatables/datatables.all.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('backend/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('backend/plugins/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('backend/js/dict/news-list.js')}}"></script>
+<script type="text/javascript" src="{{asset('backend/js/news/news-list.js')}}"></script>
 <script type="text/javascript" src="{{asset('backend/plugins/layer/layer.js')}}"></script>
 <script type="text/javascript">
   $(function() {
