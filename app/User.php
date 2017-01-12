cfd56f94c9d4c5c964a9058883020f6c21a268db
shaolei <ysl90s@qq.com>
2017-01-12 17:25:53 +0800

    fix

--- app/User.php
+++ app/User.php
@@ -26,7 +26,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','status','confirm_email'
+        'name', 'email', 'password','status','confirm_email, mobile'
     ];
 
     /**
