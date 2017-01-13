291781b517bb8c5e13a61668b9362eb19aa3c2c7
shaolei <ysl90s@qq.com>
2017-01-13 20:40:25 +0800

    fix

--- resources/views/admin/order/check.blade.php
+++ resources/views/admin/order/check.blade.php
@@ -119,7 +119,7 @@
     <script type="text/javascript" src="{{asset('backend/plugins/datatables/datatables.all.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('backend/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js')}}"></script>
     <script type="text/javascript" src="{{asset('backend/plugins/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
-    <script type="text/javascript" src="{{asset('backend/js/order/order-list.js')}}"></script>
+    <script type="text/javascript" src="{{asset('backend/js/order/check-list.js')}}"></script>
     <script type="text/javascript" src="{{asset('backend/plugins/layer/layer.js')}}"></script>
     <script type="text/javascript">
         $(function() {
