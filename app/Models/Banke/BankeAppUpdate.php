e129224527cd1153782b218c04e6cda023912240
shaolei <ysl90s@qq.com>
2017-01-22 09:47:28 +0800

    fix

--- app/Models/Banke/BankeAppUpdate.php
+++ app/Models/Banke/BankeAppUpdate.php
@@ -9,8 +9,6 @@ class BankeAppUpdate extends Model
     //
     use ActionAttributeTrait;
 
-    protected $primaryKey = 'version_code';
-
     protected $table = 'banke_app_update';
 
     protected $fillable = ['version_name', 'instruction', 'url', 'status'];
