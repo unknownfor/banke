6e10ed13788bda3a0c78c3c64f01b94054f71944
jimmy <jiangjianming_m@163.com>
2017-03-08 14:42:50 +0800

    add log info for course search

--- app/Repositories/admin/CourseRepository.php
+++ app/Repositories/admin/CourseRepository.php
@@ -31,7 +31,7 @@ class CourseRepository
 
 		$course = new BankeCourse;
 		$org = new BankeOrg;
-		Log::info('-------------------------哈哈谷侃侃保人');
+		Log::info('-------------------------哈哈谷侃侃保人-----'+$course_name);
 		/*课程名称搜索*/
 		if($course_name){
 			Log::info('-------------------------'+$course_name);
