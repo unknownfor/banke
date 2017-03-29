f87c2dae328c38d7670a3c164b20198c5ec3ab4c
mayoi <1299824519@qq.com>
2017-03-29 18:40:30 +0800

    下班前的提交

--- resources/views/web/rule/rule.blade.php
+++ resources/views/web/rule/rule.blade.php
@@ -16,77 +16,23 @@
 	<title>首页规则</title>
 </head>
 <body>
-<img class="banner" src="/front/assets/img/rule/bg.png"/>
-<!--关于半课-->
-<div class="about-banke content">
-	<div class="title">
-		<div class="about-banke-img icon"></div>
-		<span class="text-title">什么是半课</span>
-	</div>
-	<div class="text">
-		<div class="text-box">
-			半课是武汉迅牛科技联合一批优质培训机构重磅推出的学习保障激励平台，学员在半课上选择机构学习不仅可以得到品质保障、七天退款和就业服务，还可以获得最高学费50%的奖励。</div>
-	</div>
-</div>
-<!--半课服务-->
-<div class="service content">
-	<div class="title">
-		<span class="service-img icon"></span>
-		<span class="text-title">半课服务</span>
-	</div>
-	<div class="text">
-		<div class="second-title">品质保证</div>
-		<div class="text-box">
-			平台上的机构均经过半课严格筛选，考核范围包含且不限于营业执照、营业场所、师资力量、学习环境、就业服务等。平台所推荐机构
-			皆为优质培训机构，课程质量有保障。
+<div id="rule">
+	<img src="/front/assets/img/rule/banner.png">
+	<div class="content service">
+		<div class="title">
+			<span class="title-img service-img"></span>
+			<span class="title-txt">半课有哪些服务</span>
 		</div>
-		<div class="second-title">七天退款</div>
-		<div class="text-box">
-			学员在半课推荐的机构报名并且缴纳学费，即可享受7天内对课程不满意退款服务，届时请联系半课客服。(退回学费中扣除已领取奖励部分)
-		</div>
-		<div class="second-title">就业服务</div>
-		<div class="text-box">
-			半课会定期举办招聘会，并且将有一对一的招聘顾问为您服务。学成后将会为学员推荐企业应聘。
-		</div>
-		<div class="second-title">学习奖励</div>
-		<div class="text-box">
-			学员在课程结束前，每次课时都可以领取到一笔学习奖励，累计奖励最高可达到缴纳学费的50%。
-		</div>
-	</div>
-</div>
-<!--奖励流程-->
-<div class="process content">
-	<div class="title">
-		<div class="process- icon"></div>
-		<span class="text-title">奖励流程</span>
-	</div>
-	<div class="text">
-		<div class="text-box">
-			<div><span class="number">1</span>点击“我的”，进行实名认证</div>
-			<br />
-			<div><span class="number">2</span>在半课上选择机构学习并到线下缴纳学费</div>
-			<br />
-			<div><span class="number">3</span>机构提交学员报名信息至半课平台</div>
-			<br />
-			<div><span class="number">4</span>半课工作人员为该学员开通学习奖励计划</div>
-			<br />
-			<div><span class="number">5</span>上课时在首页打卡领取奖励</div>
-		</div>
-	</div>
-</div>
-<!--奖励规则-->
-<div class="reward content">
-	<div class="title">
-		<div class="process- icon"></div>
-		<span class="text-title">奖励规则</span>
-	</div>
-	<div class="text">
-		<div class="text-box">
-			<div>1.学员按照课时安排到达指定地点上课学习，App首页签到打卡领取当次课时奖励，旷课者则不享受本次课时的奖励。</div>
-			<br />
-			<div>2.课程结束时，若全勤可获得学费总额20%的奖励金额。</div>
-			<br />
-			<div>3.完成任务中心里的任务最高可获得额外30%的学费奖励。</div>
+		<div class="service-box">
+			<div class="service-detail">
+				<div class="service-title">
+					<span class="title-img org"></span>
+					<span class="title-txt">精选机构</span>
+				</div>
+				<div class="service-info">
+					严选职业培训行业优质机构，保证机构教学质量。
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
