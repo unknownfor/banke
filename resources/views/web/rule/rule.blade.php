bd12577828f1cafd85dfcaba24f9a002c63943c6
mayoi <1299824519@qq.com>
2017-03-30 10:42:41 +0800

    finished rule pagee

--- resources/views/web/rule/rule.blade.php
+++ resources/views/web/rule/rule.blade.php
@@ -33,8 +33,62 @@
 					严选职业培训行业优质机构，保证机构教学质量。
 				</div>
 			</div>
+			<div class="service-detail">
+				<div class="service-title">
+					<span class="title-img price"></span>
+					<span class="title-txt">学费减半</span>
+				</div>
+				<div class="service-info">
+					每日打卡和完成指定任务最高可领取50%学费返现。
+				</div>
+			</div>
+			<div class="service-detail">
+				<div class="service-title">
+					<span class="title-img guarantee"></span>
+					<span class="title-txt">七天担保</span>
+				</div>
+				<div class="service-info">
+					在带有<span>七天担保</span>的机构报名学习，7天不满意可以退款。
+				</div>
+			</div>
+			<div class="service-detail">
+				<div class="service-title">
+					<span class="title-img job"></span>
+					<span class="title-txt">就业服务</span>
+				</div>
+				<div class="service-info">
+					人才入库，智能匹配名企对口岗位，招聘会优先入场。
+				</div>
+			</div>
 		</div>
 	</div>
+	<div class="content payback">
+		<div class="title">
+			<span class="title-img payback-img"></span>
+			<span class="title-txt">学费怎么返现</span>
+		</div>
+		<div class="payback-box">
+			<div class="payback-detail">
+				<div class="payback-title">每日打卡</div>
+				<div class="payback-info">
+					缴纳学费后，每日在半课APP首页打卡领取当日返现，直至学习结束当天，打卡奖励金额领取完毕。
+				</div>
+			</div>
+			<div class="payback-detail">
+				<div class="payback-title">任务奖励</div>
+				<div class="payback-info">
+					<p><span>1</span>注册认证</p>
+					<p><span>2</span>邀请好友注册认证</p>
+					<p><span>3</span>邀请好友成功报名</p>
+					<p><span>4</span>完成连续打卡签到任务</p>
+					<p><span>5</span>成功报名后写评论</p>
+					<p><span>6</span>学习过程中写课程心得</p>
+					<div>(更多奖励持续开发中...)</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<p class="statement">半课官方拥有最终解释权</p>
 </div>
 </body>
 </html>
