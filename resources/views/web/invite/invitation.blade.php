77a279e03af72ebecfddd8315e071fc3b92d75c5
mayoi <1299824519@qq.com>
2017-03-31 18:44:50 +0800

    modified invitation style

--- resources/views/web/invite/invitation.blade.php
+++ resources/views/web/invite/invitation.blade.php
@@ -21,30 +21,36 @@
 <input type="hidden" name="welcome" value="{{$welcome}}"/>
 <div id="invitation">
     <p class="slogen">注册即刻领取20元现金奖励</p>
-    <p class="detail">接受好友邀请，上半课报名学习可省5000元学费</p>
+    <p class="detail">接受好友邀请,上半课报名学习可省5000元学费</p>
     <img class="bg" src="/front/assets/img/invitation/bg_1.png" />
     <form class="register">
         {{--手机号--}}
-        <div class="register-box phone">
-            <i class="iconfont">&#xe659;</i>
+        <div class="register-box">
+            <i class="iconfont register-img">&#xe659;</i>
             <input class="register-code" id="phone-num" placeholder="输入手机号"/>
         </div>
         {{--验证码--}}
-        <div class="register-box number">
-            <i class="iconfont" >&#xe620;</i>
-            <input class="register-code" id="phone-num" placeholder="验证码"/>
+        <div class="register-box">
+            <i class="iconfont register-img">&#xe6a0;</i>
+            <input class="register-code" id="user-code" placeholder="验证码"/>
             <hr color="#9b9b9b" />
             <input class="code-btn disabled" type="button" id="phone-code-btn" value="获取验证码"/>
         </div>
         {{--密码--}}
-        <div class="register-box password">
-            <i class="iconfont" >&#xe6a0;</i>
-            <input class="register-code" id="phone-num" placeholder="密码"/>
+        <div class="register-box">
+            <i class="iconfont register-img">&#xe655;</i>
+            <input class="register-code" id="password-num" placeholder="密码"/>
         </div>
-        <button class="btn"><span>领取奖励</span></button>
+        <button class="btn">领取奖励</button>
     </form>
+    <div class="down">
+        <span>
+           <i class="iconfont down-btn">&#xe7a5;</i>
+        </span>
+    </div>
+    <img class="bg second" src="/front/assets/img/invitation/bg_2.png" />
 </div>
-@include('web.layout.downloadbar')
+{{--@include('web.layout.downloadbar')--}}
 </body>
 <script src="/front/assets/scripts/invitaion/invitaion.js" type="text/javascript"></script>
 </html>
