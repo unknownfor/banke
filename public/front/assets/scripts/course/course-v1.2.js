243df6bf2cf75c14931716542204c2866a8d20c4
mayoi <1299824519@qq.com>
2017-04-19 09:58:20 +0800

    fix

--- public/front/assets/scripts/course/course-v1.2.js
+++ public/front/assets/scripts/course/course-v1.2.js
@@ -7,25 +7,5 @@ $(function() {
     $(document).on( window.eventName,'.address-call', function() {
 
         $('call-mask').removeClass('hide');
-
-        //«Î«Û—È÷§¬Î
-        if(countdown==60) {
-            var url = '/invitation/requestSmsCode';
-            getDataAsync(url, {mobile: $('#phone-num').val()},
-                function (res) {
-                    if(res.status_code==50016){
-                        $('.register-old').show().parent().show().siblings().hide();
-                        return;
-                    }
-                    window.showTips(res.message);
-                    if(res.status_code!=0) {
-                        countdown = 0;
-                        //setGetCodeBtn();
-                    }
-                },function(){
-                    countdown = 0;
-                    //setGetCodeBtn();
-                },'post');
-        }
     });
 });
