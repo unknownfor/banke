07561288dd2919c1780047d0863836b02d75af75
mayoi <1299824519@qq.com>
2017-05-08 18:01:35 +0800

    modified style

--- public/front/assets/scripts/invitaion/enrol.v1.3.js
+++ public/front/assets/scripts/invitaion/enrol.v1.3.js
@@ -1,15 +1,15 @@
 /**
  * Created by jimmy-hisihi on 2017/5/6.
  */
-var url='/v1.3/share/doenrol',
-    data={
-        org_id:12,
-        course_id:30,
-        invitation_uid:210,
-        mobile:'18140662282',
-        org_name:'武汉卓意设计职业培训学校',
-        course_name:'网页设计全科班'
-    };
-window.getDataAsync(url,data,function(res) {
-    res;
-},null,'post');
+// var url='/v1.3/share/doenrol',
+//     data={
+//         org_id:12,
+//         course_id:30,
+//         invitation_uid:210,
+//         mobile:'18140662282',
+//         org_name:'武汉卓意设计职业培训学校',
+//         course_name:'网页设计全科班'
+//     };
+// window.getDataAsync(url,data,function(res) {
+//     res;
+// },null,'post');
--- resources/views/web/invite/enrol-v1_3.blade.php
+++ resources/views/web/invite/enrol-v1_3.blade.php
@@ -13,20 +13,21 @@
     <meta http-equiv="Cache-control" content="no-cache">
     <meta http-equiv="Cache" content="no-cache">
     <link href="/front/assets/css/invitation/v1.3/invitation.css" rel="stylesheet" type="text/css"/>
+    <link href="/front/assets/css/invitation/v1.3/iconfont/iconfont.css" rel="stylesheet" type="text/css"/>
     <title>{{$course['name']}}</title>
 </head>
 <body>
 {!! csrf_field() !!}
 <div id="invitation">
-    <div class="box user">
+    <div class="box user hide">
         <div class="user-left">
             <img src="{{$user['avatar']}}@70h_70w_2e">
         </div>
         <div class="user-right">
-            <p>您的好友<span class="user-friend">{{$user['name']}}</span>已经报名<br />他帮您省去了<span class="color">一半学费</p>
+            <p>您的好友<span class="user-friend">{{$user['name']}}</span>已经报名<br />他帮您免去了<span class="color">一半学费</p>
         </div>
     </div>
-    <div class="box info">
+    <div class="box info hide">
         <div class="org-name">{{$org['name']}}</div>
         <div class="class">
             <div class="class-left">
@@ -36,16 +37,42 @@
                 <div class="name">{{$course['name']}}</div>
                 <div class="save">
                     <span class="save-img"></span>
-                    <span class="save-num">{{$course['discount']}}%</span>
+                    <span class="save-num">{{$course['checkin_award']}}%</span>
                 </div>
             </div>
             <div class="class-right">
                 <div class="price"><span class="price-info">参考价</span><span id="price-num">￥{{$course['price']}}</span></div>
-                <div class="real-price"><span class="price-info">最高奖励金额</span><span id="price-real-num">￥{{$course['real_price']}}</span></div>
+                <div class="real-price"><span class="price-info">最高奖励金额</span><span id="price-real-num">￥{!! $course['price'] * $course['checkin_award']/100!!}</span></div>
             </div>
         </div>
-
     </div>
+    <div class="box join hide">
+        <p class="slogen">用一半学费上好课！</p>
+        <div class="btn">和好友一起学习</div>
+    </div>
+    <div class="box rule hide">
+        <div class="txt"><span>如何只花一半学费上好课</span></div>
+        <div class="link"><a href="javascript:void 0;">更多<i class="iconfont">&#xe600;</i></a></div>
+    </div>
+    <div class="box1 process">
+        <h3>报名流程</h3>
+        <div class="rules first"><div class="num">1</div><div class="txt">留下您的联系方式</div></div>
+        <i class="iconfont dotted">&#xe603;</i>
+        <div class="rules"><div class="num">2</div><div class="txt">去往该培训机构缴纳学费</div></div>
+        <i class="iconfont dotted">&#xe603;</i>
+        <div class="rules"><div class="num">3</div><div class="txt">在机构填写信息（与平台认证信息一致）</div></div>
+        <i class="iconfont dotted">&#xe603;</i>
+        <div class="rules"><div class="num">4</div><div class="txt">平台核实后，在首页每日打卡领取学费</div></div>
+    </div>
+    <div class="box1 register">
+        <div class="phone">
+            <i class="iconfont register-img">&#xe659;</i>
+            <input class="register-code" id="phone-num" placeholder="输入手机号"/>
+        </div>
+        <button class="btn nouse"><span>确定</span></button>
+        <p>客服将会马上联系你</p>
+    </div>
+
 </div>
 </body>
 <script src="/front/assets/plugins/zepto.min.js"></script>
