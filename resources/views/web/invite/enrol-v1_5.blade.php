423c3b3e10bf55a24b3c1dd52483a01dd34d620c
mayoi <1299824519@qq.com>
2017-06-01 16:39:29 +0800

    modified style

--- resources/views/web/invite/enrol-v1_5.blade.php
+++ resources/views/web/invite/enrol-v1_5.blade.php
@@ -30,8 +30,8 @@
             <?php
             $imgs=explode(',',$word['img_url_web']);
             ?>
-            {{--<img class="slogen-bg" src="{{$word['img_url_web']}}" />--}}
-            <img class="slogen-bg" src="http://pic.hisihi.com/2017-05-31/1496211559475752.png" />
+            <img class="slogen-bg" src="{{$word['img_url_web']}}" />
+            {{--<img class="slogen-bg" src="http://pic.hisihi.com/2017-05-31/1496211559475752.png" />--}}
         @else
             <img class="head-bg" src="{{asset('front/assets/img/org/banke-org.png')}}" />
         @endif
