c4ad93a1e19626e7ea270a76f098dfe770c065b9
mayoi <1299824519@qq.com>
2017-07-06 18:17:10 +0800

    add enrol page & modified enrol page style

--- resources/views/web/invite/enrol-v1_7.blade.php
+++ resources/views/web/invite/enrol-v1_7.blade.php
@@ -19,161 +19,70 @@
 <body>
 {!! csrf_field() !!}
 <div id="enrol">
-    <div class="box user"
-         data-uid="{{$user['uid']}}"
-         data-course-id="{{$course['id']}}"
-         data-org-id="{{$org['id']}}"
-         data-record-id="{{$shareInfo['record_id']}}"
-         data-type-id="{{$shareInfo['type_id']}}"
-    >
-        @if($word['img_url_web'])
-            <?php
-            $imgs=explode(',',$word['img_url_web']);
-            ?>
-            <img class="slogen-bg" src="{{$word['img_url_web']}}" />
-        @else
-            <img class="head-bg" src="{{asset('front/assets/img/org/banke-org.png')}}" />
-        @endif
-
-
-        <div class="user-info">
-            <div class="info-left">
-                <div class="user-img">
-                    <img src="{{$user['avatar']}}@40h_40w_2e" />
+    <div class="page">
+        <div class="container">
+            <div class="head-txt">
+                <div class="txt-one">成为优质机构代言人</div>
+                <div class="txt-two">一大波福利等着你</div>
+            </div>
+            <img class="head-img first" src="/front/assets/img/invitation/v1.7/coo.png" />
+            <from class="input-box">
+                <div class="input-info">
+                    <input placeholder="输入手机号" />
                 </div>
-                <div class="user-title"></div>
-            </div>
-
-            <div class="info-right">
-                <div class="user-name"><span>{{$user['name']}}</span>说:</div>
-            </div>
-        </div>
-
-
-
-    </div>
-    <div class="box information">
-        <div class="box org-name">{{$org['name']}}</div>
-        <div class="head">
-            <div class="head-left">
-                <a href="{{$course['link_url']}}">
-                <img src="{{$course['cover']}}@80h_80w_2e" />
-                </a>
-            </div>
-            <div class="head-middle">
-                <div class="name">{{$course['name']}}</div>
-                <div class="save">
-                    <span class="save-img"></span>
-                    <span class="save-num">{{$course['max_award_percent']}}%</span>
+                <div class="input-info">
+                    <input placeholder="验证码" />
+                    <hr/>
+                    <input class="code-btn" placeholder="获取验证码">
                 </div>
-            </div>
-            <div class="head-right">
-                <div class="price"><span class="price-info">原价</span><span id="price-num">￥{{$course['price']}}</span></div>
-                <div class="real-price"><span class="price-info">报名后最高返</span><span id="price-real-num">￥{{$course['max_award']}}</span></div>
-            </div>
-        </div>
-    </div>
-    <div class="box line"></div>
-    <div class="box join">
-        <div class="join-people">
-            <div class="leader">
-                <div class="user-img">
-                    <img src="{{$user['avatar']}}@40h_40w_2e" />
+
+                <div class="input-info">
+                    <input  placeholder="密码" type="password"/>
                 </div>
-                <div class="user-title"></div>
+
+                <button class="btn">
+                    <a class="downloadBar" href="http://www.91banke.com/web/download">成为机构代言人</a>
+                </button>
+            </from>
+
+        </div>
+    </div>
+    <div class="page">
+        <div class="container">
+            <img class="head-img" src="/front/assets/img/invitation/v1.7/coo1.png" />
+            <div class="head-txt">
+                <div class="txt-one">下载半课</div>
+                <div class="txt-two">成为优质机构代言人</div>
             </div>
-
-            @if($members)
-                @foreach($members['data'] as $v)
-                    <div class="follower">
-                            <img src="{{$v['avatar']}}" />
-                    </div>
-                @endforeach
-            @else
-                <div class="follower" style="display: none"></div>
-            @endif
-
-            <div id="more">
-                <img src="/front/assets/img/invitation/v1.5/head.png" />
+            <div class="btn">
+                <a class="downloadBar" href="http://www.91banke.com/web/download">下载半课</a>
             </div>
-
-        </div>
-        <div class="join-slogen">已有<span>{{$members['counts']}}人</span>预约参团，赶快参团吧</div>
-        <div class="join-btn">立即参团</div>
-        <div class="join-description">
-            <div class="title">参团须知</div>
-            <div class="detail">团长，每参团成功一人，团长获得<span> {{$award['organizer_award']}}元返现</span></div>
-            <div class="detail">团员，参团成功后，最高可获得<span> {{$course['max_award']}}元返现</span></div>
         </div>
     </div>
-    <div class="box line"></div>
-    <div class="box join">
-        <div class="join-description">
-            <div class="title">什么是半课</div>
-            <div class="detail">一半学费上好课，为学生提供7天担保服务，学生通过打卡获得返现，开团享受优惠，
-                最高获得50%的返现帮助学生筛选出更具性价比的课程及机构，为学生提供终生学习的服务。</div>
-        </div>
-        <div class="join-rule">
-            <a href="{{$ruleLinkUrl}}">了解返现规则</a>
-        </div>
-    </div>
-
-    <div class="box line"></div>
-    <div class="box cooperation">
-        <div class="title">优质合作机构推荐</div>
-        <div class="org-container">
-            @if($superiororg)
-                @foreach($superiororg as $v)
-                    <div class="coo-org">
-                        <a href="{{$v->url}}">
-                            <div class="tip-box">
-                                <div class="tip-img"></div>
-                                <div class="tip-txt">{{$v->category}}</div>
-                            </div>
-                            <div class="org-box">
-                                <img class="org-img" src="{{$v->logo}}"/>
-                                <div class="org-name">{{$v->name}}</div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-            @endif
+    <div class="page">
+        <div class="container">
+            <img class="head-img" src="/front/assets/img/invitation/v1.7/coo2.png" />
+            <div class="head-txt">
+                <div class="txt-one">邀请招生老师</div>
+                <div class="txt-two">可获得100元返现</div>
+            </div>
+            <div class="btn">
+                <a class="downloadBar" href="http://www.91banke.com/web/download">下载半课</a>
+            </div>
         </div>
     </div>
-
-    <div class="box1 process hide">
-        <h3>报名流程</h3>
-        <div class="rules first"><div class="num">1</div><div class="txt">留下您的联系方式</div></div>
-        <i class="iconfont dotted">&#xe603;</i>
-        <div class="rules"><div class="num">2</div><div class="txt">去往该培训机构缴纳学费</div></div>
-        <i class="iconfont dotted">&#xe603;</i>
-        <div class="rules"><div class="num">3</div><div class="txt">在机构填写信息（与平台认证信息一致）</div></div>
-        <i class="iconfont dotted">&#xe603;</i>
-        <div class="rules"><div class="num">4</div><div class="txt">平台核实后，在首页每日打卡领取学费</div></div>
-    </div>
-
-    <div class="box1 line hide"></div>
-    <div class="box1 register hide">
-        <div class="phone">
-            <i class="iconfont register-img">&#xe659;</i>
-            <input class="register-code" id="phone-num" placeholder="输入手机号"/>
-        </div>
-        <button class="btn nouse" id="register-btn"><span>确定</span></button>
-        <p>客服将会马上联系你</p>
-    </div>
-
-    <div class="container hide" >
-        <img class="bg second" src="http://pic.hisihi.com/2017-04-14/1492163041837846.png" />
-        <div class="txt txt-one">早点报名，早点免学费哟！</div>
-        <div class="txt txt-two">最高可领取<span>50%</span>学费返现</div>
-        <div class="btn active"><a href="http://www.91banke.com/web/download">下载半课，体验学费返现</a></div>
-    </div>
-
-
 </div>
 </body>
 <script src="/front/assets/plugins/zepto.min.js"></script>
+<script src="/front/assets/plugins/zepto_modify.js"></script>
+<script src="/front/assets/plugins/PageSlider.js"></script>
 <script src="/front/assets/plugins/fastclick.js" type="text/javascript"></script>
 <script src="/front/assets/plugins/common.js" type="text/javascript"></script>
 <script src="/front/assets/scripts/invitaion/enrol.v1.7.js" type="text/javascript"></script>
+<script>
+    new PageSlider({
+        pages: $('#enrol .page'),
+        gestureFollowing: true
+    });
+</script>
 </html>
