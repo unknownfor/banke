5ebd82c7a229f266d1bc068101fd91e8005bae43
mayoi <1299824519@qq.com>
2017-07-28 10:45:57 +0800

    新增返现说明和退款说明

--- resources/views/web/refund/share_refund-v1_8.blade.php
+++ resources/views/web/refund/share_refund-v1_8.blade.php
@@ -12,17 +12,45 @@
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Cache-control" content="no-cache">
     <meta http-equiv="Cache" content="no-cache">
-    <link type="text/css" href="/front/assets/css/course/v1.8/course.css" rel="stylesheet">
-    <link type="text/css" href="/front/assets/css/course/v1.8/iconfont/iconfont.css" rel="stylesheet">
-    <title>refund share1.8</title>
+    <link type="text/css" href="/front/assets/css/refund/v1.8/refund.css" rel="stylesheet">
+    <title>退款说明</title>
 </head>
 <body>
-refund1.8 share
+<div id="wrapper">
+    <div class="container">
+        <div class="box">
+            <div class="title">
+                <span class="title-img how"></span>
+                <span class="txt">如何退款？</span>
+            </div>
+            <div class="underline"></div>
+            <div class="info">点击APP右下角的-【我的】-【我的定单】-【申请退款】，工作人员会第一时间联系您，核实信息</div>
+        </div>
+        <div class="box">
+            <div class="title">
+                <span class="title-img when"></span>
+                <span class="txt">退款时间？</span>
+            </div>
+            <div class="underline"></div>
+            <div class="info">工作人员会在3-5工作日内联系您。</div>
+        </div>
+        <div class="box">
+            <div class="title">
+                <span class="title-img know"></span>
+                <span class="txt">退款须知</span>
+            </div>
+            <div class="underline"></div>
+            <div class="info">通过半课报名获得返现以后，在半课平台获得的返现从退款中减去。</div>
+        </div>
+        <div class="annotate">
+            <div class="tips">即：退款=课程价-已经获得的返现</div>
+        </div>
+    </div>
+</div>
 
 @include('web.layout.downloadbar')
 </body>
 <script src="/front/assets/plugins/zepto.min.js"></script>
 <script src="/front/assets/plugins/fastclick.js" type="text/javascript"></script>
 <script src="/front/assets/plugins/common.js" type="text/javascript"></script>
-<script src="/front/assets/scripts/course/course-v1.8.js" type="text/javascript"></script>
 </html>
