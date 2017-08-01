4caff0063b2411b1a8d3d03f48cee754ee74871c
mayoi <1299824519@qq.com>
2017-08-01 10:55:10 +0800

    modified course imgdetail page

--- resources/views/web/course/course_imgdetails-v1_8.blade.php
+++ resources/views/web/course/course_imgdetails-v1_8.blade.php
@@ -16,41 +16,40 @@
     <title>课程详情</title>
 </head>
 <body>
-
-[购买流程]
-
-1.下订单并支付预约金
-
-2.工作人员将第一时间联系您，让您选择机构校区及地址
-
-3.前往报名机构，缴纳剩余学费，也可以申请分期
-
-
-
-[如何咨询]
-
-1.点击页面上方的电话或者在线咨询，学习咨询师将会第
-
-一时间联系您
-
-
-
-[如何退款]
-
-1.点击APP右下角的我的，进入“我的订单”，点击“申请退款”
-
-工作人员会第一时间联系您，核实信息
-
-
-
-[购买须知]
-
-1.下单后，您还需到机构缴纳（半课价-预约金）元
-
-2.若您还有其他疑问，请在工作日9：00到22：00间，拨打
-
-半课客服电话4000340033或者在线咨询
-
+<div id="course">
+    <div class="description container">
+        <img src="/front/assets/img/course/v1.8/banner.jpeg" />
+        <div class="des-txt">
+            <div class="txt-box">
+                <div class="title">购买流程</div>
+                <div class="info">
+                    <p>1.下订单并支付预约金</p>
+                    <p>2.工作人员将第一时间联系您，让您选择机构校区及地址</p>
+                    <p>3.前往报名机构，缴纳剩余学费，也可以申请分期</p>
+                </div>
+            </div>
+            <div class="txt-box">
+                <div class="title">如何咨询</div>
+                <div class="info">
+                    <p>点击页面上方的电话或者在线咨询，学习咨询师将会第一时间联系您</p>
+                </div>
+            </div>
+            <div class="txt-box">
+                <div class="title">如何退款</div>
+                <div class="info">
+                    <p>点击APP右下角的我的，进入“我的订单”，点击“申请退款”,工作人员会第一时间联系您，核实信息</p>
+                </div>
+            </div>
+            <div class="txt-box">
+                <div class="title">购买须知</div>
+                <div class="info">
+                    <p>1.下单后，您还需到机构缴纳（半课价-预约金）元</p>
+                    <p>2.若您还有其他疑问，请在工作日9：00到22：00间，拨打半课客服电话4000340033或者在线咨询</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </body>
 <script src="/front/assets/plugins/zepto.min.js"></script>
 <script src="/front/assets/plugins/fastclick.js" type="text/javascript"></script>
