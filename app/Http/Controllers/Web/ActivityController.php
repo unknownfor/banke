a3526387d962e0b4e939b95e4ad4851adc5f9312
jimmy <jiangjianming_m@163.com>
2017-08-10 14:43:49 +0800

    modified activity detail page

--- app/Http/Controllers/Web/ActivityController.php
+++ app/Http/Controllers/Web/ActivityController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use OrgRepository;
 use ActivityRepository;
 use Illuminate\Support\Facades\Log;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use Validator;
 use Illuminate\Http\Request;
+use App\Models\Banke\BankeActivity;
 
 class ActivityController extends Controller
 {
@@ -18,7 +18,8 @@ class ActivityController extends Controller
     */
     public function v1_8($id)
     {
-        return view('web.activity.v1_8');
+        $activity = BankeActivity::find($id);
+        return view('web.activity.v1_8')->with(compact(['activity']));
     }
 
 
@@ -27,6 +28,7 @@ class ActivityController extends Controller
      */
     public function share_v1_8($id)
     {
-        return view('web.activity.share_v1_8');
+        $activity = BankeActivity::find($id);
+        return view('web.activity.share_v1_8')->with(compact(['activity']));
     }
 }
