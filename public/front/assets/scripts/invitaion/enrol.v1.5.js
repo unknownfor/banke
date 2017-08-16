f1915b8ff1de197c8024de400f8e7b2ddabb7269
jimmy <jiangjianming_m@163.com>
2017-08-16 17:00:56 +0800

    modified org course comment bug

--- public/front/assets/scripts/invitaion/enrol.v1.5.js
+++ public/front/assets/scripts/invitaion/enrol.v1.5.js
@@ -108,18 +108,16 @@ $(function (){
      id   表示记录id
      * */
     function viewCounts() {
-        for(var i=0;i<10;i++) {
-            var box = $('.user'),
-                typeId = box.attr('data-type-id'),
-                id = box.attr('data-record-id'),
-                url = '/v1.5/share/updateviewcounts',
-                data = {
-                    typeid: typeId,
-                    id: id
-                }
-            getDataAsync(url, data, function () {
+        var box = $('.user'),
+            typeId = box.attr('data-type-id'),
+            id = box.attr('data-record-id'),
+            url = '/v1.5/share/updateviewcounts',
+            data = {
+                typeid: typeId,
+                id: id
+            }
+        getDataAsync(url, data, function () {
 
-            }, null, 'post');
-        }
+        }, null, 'post');
     };
 });
