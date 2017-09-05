b00c7af7d5a3f0463d1f43063f35257eb76cddb2
jimmy <jiangjianming_m@163.com>
2017-09-05 22:00:51 +0800

    add groupbuying award for new task

--- app/Repositories/admin/TaskFormDetailUserRepository.php
+++ app/Repositories/admin/TaskFormDetailUserRepository.php
@@ -5,6 +5,7 @@ use Flash;
 use App\Models\Banke\BankeTaskFormUser;
 use App\Models\Banke\BankeTaskFormDetailUser;
 use App\Models\Banke\BankeTask;
+use App\Models\Banke\BankeTaskCard;
 use Illuminate\Support\Facades\Log;
 use DB;
 
@@ -19,7 +20,9 @@ class TaskFormDetailUserRepository
 	public static function updataTaskFormDetailUser($id){
 		$user=BankeTaskFormDetailUser::find($id);
 		$user->status=2;
+		$user->times_finished=date('Y-m-d H:i:s');
 		$user->save();
+		self::addPatchCard($user['user_id']);
 	}
 
 	/*
@@ -30,11 +33,32 @@ class TaskFormDetailUserRepository
 		$user=BankeTaskFormDetailUser::find($id);
 		$user->award_coin=$award;
 		$user->status=2;
+		$user->times_finished=date('Y-m-d H:i:s');
 		$user->save();
+		self::addPatchCard($user['user_id']);
 	}
 
-	public static function addPatchCard($uid)
+	/*领取补领卡*/
+	private static function addPatchCard($uid)
+	{
+		$uid=1;
+		$card=BankeTaskCard::where('user_id',$uid);
+		if($card->count()==0){
+			$card = new BankeTaskCard;
+			$card->card_count=0.5;
+			$card->user_id=$uid;
+			$card->save();
+		}else{
+			$card=$card->first();
+			$card->card_count+=0.5;
+			$card->save();
+		}
+	}
+
+	/*统一更新用户的任务完成情况*/
+	private static  function updataUserFinishStatus($uid,$type)
 	{
 
 	}
+
 }
